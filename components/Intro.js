99da47c3166822c19252475d6ba273d52296fc04
Somanyu <somanyu.03samal@gmail.com>
2022-10-13 21:11:02 +0530

    Improved Responsiveness 🎨

--- components/Intro.js
+++ components/Intro.js
@@ -3,18 +3,18 @@ import somanyu from '../public/images/somanyu.jpg'
 
 export default function Intro() {
     return (
-        <div class="container" style={{marginTop: "4rem"}}>
+        <div class="container" style={{ marginTop: "4rem", paddingTop: "4.5rem" }}>
             <div class="row">
-                <div class="col-md-6 col-lg-3 offset-lg-0 d-flex d-sm-flex d-md-flex d-lg-flex justify-content-center align-items-center justify-content-sm-center align-items-sm-center order-sm-first justify-content-md-center align-items-md-center order-md-first justify-content-lg-center align-items-lg-center order-lg-last">
+                <div class="col-md-6 col-lg-4 offset-lg-0 d-flex d-sm-flex d-md-flex d-lg-flex justify-content-center align-items-center justify-content-sm-center align-items-sm-center order-sm-first justify-content-md-center align-items-md-center order-md-first justify-content-lg-center align-items-lg-center order-lg-last">
                     <div class="d-lg-flex justify-content-lg-center align-items-lg-center">
-                        <Image class="img-fluid" src={somanyu} alt="Somanyu" width="139" height="151" style={{borderRadius: "2px"}} />
+                        <Image class="img-fluid" src={somanyu} alt="Somanyu" width="204" height="221" style={{ borderRadius: "2px" }} />
                     </div>
                 </div>
-                <div class="col-md-6 col-lg-4 offset-lg-3 order-sm-last order-md-last order-lg-first">
-                    <div class="box" style={{background: "rgba(255,255,255,0)"}}>
-                        <h1 class="display-4 name" style={{color: "#ffffff",fontFamily: "Questrial, sans-serif",textAlign: "left",paddingTop: "3px",marginTop: "0px",marginBottom: "0px",lineHeight: "60.8px",marginRight: "0px",paddingRight: "0px",paddingBottom: "0px"}}>Somanyu</h1>
-                        <p class="title" style={{color: "#ededed",fontFamily: "'IBM Plex Sans', sans-serif",marginBottom: "12px"}}>Full-stack Developer</p>
-                        <p style={{color: "#ababab",fontFamily: "'IBM Plex Sans', sans-serif",marginLeft: "0px",paddingRight: "0px",marginRight: "0px"}}>Studying computer science, aspiring full-stack developer and artsy type.</p>
+                <div class="col-md-6 col-lg-4 offset-lg-2 d-lg-flex order-sm-last order-md-last align-items-lg-center order-lg-first">
+                    <div class="box" style={{ background: "rgba(255,255,255,0)" }}>
+                        <h1 class="display-4 name" style={{ color: "#ffffff", fontFamily: "Questrial, sans-serif", textAlign: "left", paddingTop: "3px", marginTop: "0px", marginBottom: "0px", lineHeight: "60.8px", marginRight: "0px", paddingRight: "0px", paddingBottom: "0px" }}>Somanyu</h1>
+                        <p class="title" style={{ color: "#ededed", fontFamily: "'IBM Plex Sans', sans-serif", marginBottom: "12px" }}>Full-stack Developer</p>
+                        <p style={{ color: "#ababab", fontFamily: "'IBM Plex Sans', sans-serif", marginLeft: "0px", paddingRight: "0px", marginRight: "0px" }}>Studying computer science, aspiring full-stack developer and artsy type.</p>
                     </div>
                 </div>
             </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,10 @@
 import Script from "next/script"
 import Link from "next/link"
-import styles from './Navbar.module.css'
 
 export default function Navbar() {
     return (
 
-        <nav class="navbar navbar-light navbar-expand-md py-3">
+        <nav class="navbar navbar-light fixed-top navbar-expand-md py-3" style={{ marginTop: "-10px" }}>
             <div class="container">
                 <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-3" style={{ background: "#fff" }}>
                     <span class="visually-hidden">Toggle navigation</span>
@@ -15,22 +14,22 @@ export default function Navbar() {
                     <ul class="navbar-nav mx-auto">
                         <li class="nav-item">
                             <Link href="#">
-                                <a class="nav-link" style={{ color: "rgba(255,255,255,0.9)", fontFamily: "'IBM Plex Sans', sans-serif"}}>Home</a>
+                                <a class="nav-link" style={{ color: "#b1b1b3", fontFamily: "'IBM Plex Sans', sans-serif" }}>Home</a>
                             </Link>
                         </li>
                         <li class="nav-item">
                             <Link href="#">
-                                <a class="nav-link" style={{ color: "rgba(255,255,255,0.9)", fontFamily: "'IBM Plex Sans', sans-serif"}}>Experience</a>
+                                <a class="nav-link" style={{ color: "#b1b1b3", fontFamily: "'IBM Plex Sans', sans-serif" }}>Experience</a>
                             </Link>
                         </li>
                         <li class="nav-item">
                             <Link href="#">
-                                <a class="nav-link" style={{ color: "rgba(255,255,255,0.9)", fontFamily: "'IBM Plex Sans', sans-serif"}}>Dashboard</a>
+                                <a class="nav-link" style={{ color: "#b1b1b3", fontFamily: "'IBM Plex Sans', sans-serif" }}>Dashboard</a>
                             </Link>
                         </li>
                         <li class="nav-item">
                             <Link href="#">
-                                <a class="nav-link" style={{ color: "rgba(255,255,255,0.9)", fontFamily: "'IBM Plex Sans', sans-serif"}}>Snippets</a>
+                                <a class="nav-link" style={{ color: "#b1b1b3", fontFamily: "'IBM Plex Sans', sans-serif" }}>Snippets</a>
                             </Link>
                         </li>
 
@@ -40,7 +39,8 @@ export default function Navbar() {
                     <Script src="https://cdn.lordicon.com/pzdvqjsp.js"></Script>
                     <lord-icon
                         src="https://cdn.lordicon.com/spgozyor.json"
-                        trigger="hover"
+                        trigger="loop"
+                        delay="1500"
                         style={{ width: "70px", height: "70px" }}>
                     </lord-icon>
                 </button>
