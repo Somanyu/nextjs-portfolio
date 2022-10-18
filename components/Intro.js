bb014bdd04bb4417ea5f87ccf60dd5339b1951ba
Somanyu <somanyu.03samal@gmail.com>
2022-10-18 12:06:00 +0530

    Changed Unecessary Font Family 🍻

--- components/Intro.js
+++ components/Intro.js
@@ -12,7 +12,7 @@ export default function Intro() {
                 </div>
                 <div class="col-md-6 col-lg-4 offset-lg-2 d-lg-flex order-sm-last order-md-last align-items-lg-center order-lg-first">
                     <div class="box" style={{ background: "rgba(255,255,255,0)" }}>
-                        <h1 class="display-4 name" style={{ color: "#ffffff", fontFamily: "Questrial, sans-serif", textAlign: "left", paddingTop: "3px", marginTop: "0px", marginBottom: "0px", lineHeight: "60.8px", marginRight: "0px", paddingRight: "0px", paddingBottom: "0px" }}>Somanyu</h1>
+                        <h1 class="display-4 name" style={{ color: "#ffffff", fontFamily: "'IBM Plex Sans', sans-serif", fontWeight: "bold", textAlign: "left", paddingTop: "3px", marginTop: "0px", marginBottom: "0px", lineHeight: "60.8px", marginRight: "0px", paddingRight: "0px", paddingBottom: "0px" }}>Somanyu</h1>
                         <p class="title" style={{ color: "#ededed", fontFamily: "'IBM Plex Sans', sans-serif", marginBottom: "12px" }}>Full-stack Developer</p>
                         <p style={{ color: "#ababab", fontFamily: "'IBM Plex Sans', sans-serif", marginLeft: "0px", paddingRight: "0px", marginRight: "0px" }}>Studying computer science, aspiring full-stack developer and artsy type.</p>
                     </div>
