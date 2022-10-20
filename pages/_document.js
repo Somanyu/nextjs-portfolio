d311e401b3e02a18e019f8033edc446bf3b93750
Somanyu <somanyu.03samal@gmail.com>
2022-10-20 22:11:38 +0530

    💄 Added required font & icon links

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link href="https://fonts.googleapis.com/css2?family=Amaranth&family=IBM+Plex+Sans:wght@400;600&family=Questrial&family=Rubik&display=swap" rel="stylesheet" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/css/line-awesome.min.css" integrity="sha512-vebUliqxrVkBy3gucMhClmyQP9On/HAWQdKDXRaAlb/FKuTbxkjPKUyqVOxAcGwFDka79eTF+YXwfke1h3/wfg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+            </Head>
+            <body style={{ backgroundColor: "#121212" }}>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
