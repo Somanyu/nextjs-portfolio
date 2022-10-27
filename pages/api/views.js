b58e88e01f26bef4b7a4ac473ed5355528a7115e
Somanyu <somanyu.03samal@gmail.com>
2022-10-27 11:15:48 +0530

    🚀 Deploying to Vercel

--- pages/api/views.js
+++ pages/api/views.js
@@ -1,8 +1,8 @@
 import countapi from 'countapi-js';
 
-export default function views (req, res) {
+export default function views(_, res) {
     countapi.visits('global').then((result) => {
-        const finalViews = result.value-7268;
+        const finalViews = result.value - 7268;
         res.status(200).json({ views: finalViews })
     })
 }
