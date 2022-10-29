ed86bd23bd44bd2270ae9f314874515a70f863df
Somanyu <somanyu.03samal@gmail.com>
2022-10-29 17:22:46 +0530

    💄 Image height and width responsive.

--- components/TopTrack.js
+++ components/TopTrack.js
@@ -28,7 +28,7 @@ export default function TopTrack() {
                                     <tr>
                                     {Array.from(Array(5), (e, i) => {
                                         return <td key={topTracks?.tracks[i].artist} class="d-flex d-lg-flex justify-content-start align-items-center justify-content-sm-start align-items-sm-center justify-content-md-start align-items-md-center justify-content-lg-start align-items-lg-center justify-content-xl-start align-items-xl-center justify-content-xxl-start align-items-xxl-center" style={{ marginLeft: "15px" }}>
-                                            <div><Image class="img-fluid" alt={topTracks?.tracks[i].title} src={topTracks?.tracks[i].coverImage.url} width="65" height="65" /></div>
+                                            <div><Image class="img-fluid" alt={topTracks?.tracks[i].title} src={topTracks?.tracks[i].coverImage.url} width="50" height="50" /></div>
                                             <div style={{ marginLeft: "27px" }}>
                                                 <h5 style={{ marginBottom: "0px" }}>{topTracks?.tracks[i].title}</h5>
                                                 <p style={{ fontStyle: "italic", color: "var(--fontParaColor)" }}>{topTracks?.tracks[i].artist}</p>
