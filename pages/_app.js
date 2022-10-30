cc2e79468f8f01a953aa6edaa77332f59ef2dc44
Somanyu <somanyu.03samal@gmail.com>
2022-10-30 11:50:55 +0530

    🔨 Included necessary meta tags.

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,18 @@ import { ThemeProvider } from "next-themes";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <ThemeProvider enableSystem={false} defaultTheme="dark">
-      <Component {...pageProps} />
-    </ThemeProvider>
+      <Head>
+        <meta charSet='utf-8' />
+        <title>Somanyu Samal</title>
+        <meta name="description" content="My portfolio built with Next.js, Bootstrap and Vercel 💡" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name='keywords' content='nextjs, porfolio, somanyu, somanyu samal, website portfolio' />
+        <meta name='author' content='Somanyu Samal' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </Head>
+      <ThemeProvider enableSystem={false} defaultTheme="dark">
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
