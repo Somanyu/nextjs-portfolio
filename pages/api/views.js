f0372b1627e11c5debbd197e002d8ee624633d8b
Somanyu <somanyu.03samal@gmail.com>
2022-10-30 12:27:12 +0530

    👽️ View count managed

--- pages/api/views.js
+++ pages/api/views.js
@@ -2,7 +2,7 @@ import countapi from 'countapi-js';
 
 export default function views(_, res) {
     countapi.visits('global').then((result) => {
-        const finalViews = result.value - 7268;
+        const finalViews = result.value - 7560;
         res.status(200).json({ views: finalViews })
     })
 }
